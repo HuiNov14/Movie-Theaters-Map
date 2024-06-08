d3064dfd3f1516c18f6bff66bb07bfc19b91ba20
HuiNov14 <kiritobuithanhhutluck@gmail.com>
2024-06-09 04:46:52 +0700

    update 2

--- src/AddPlace.js
+++ src/AddPlace.js
@@ -1,11 +1,36 @@
-import { StyleSheet, Text, View, TextInput, Button, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button, Dimensions } from 'react-native';
 import React, { useState, useEffect } from 'react';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Polyline } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import * as Location from 'expo-location';
+import cinemaData from './db.json';  // Import dữ liệu từ file db.json
+import axios from 'axios';
+
+const GOOGLE_MAPS_APIKEY = 'YOUR_GOOGLE_MAPS_API_KEY'; // Replace with your Google Maps API Key
 
 export default function AddPlace({ route, navigation }) {
 
-  const [currentLocation, setCurrentLocation] = useState(null);
+  const defaultLocation = () => {
+    return {
+      latitude: 10.859313791905437,
+      longitude: 106.60419726148713,
+      latitudeDelta: 0.722,
+      longitudeDelta: 0.421,
+    };
+  };
+
+  const defaultUserLocation = {
+    latitude: 10.847388306318624,
+    longitude: 106.83729892481165,
+  };
+
+  const [currentLocation, setCurrentLocation] = useState(null); // No default location
+  const [region, setRegion] = useState(defaultLocation());
+  const [formattedAddress, setFormattedAddress] = useState('');
+  const [nearestCinema, setNearestCinema] = useState(null);
+  const [theaterMarkers, setTheaterMarkers] = useState([]);
+  const [showDirections, setShowDirections] = useState(false);
+  const [routeCoords, setRouteCoords] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -18,47 +43,43 @@ export default function AddPlace({ route, navigation }) {
     })();
   }, []);
 
-  const locateUser = async () => {
-    const location = await Location.getCurrentPositionAsync({});
-    setCurrentLocation(location.coords);
-  };
-  const [region, setRegion] = useState(null);
-
-  const [formattedAddress, setFormattedAddress] = useState('');
   const getCurrentLocation = async () => {
+    setCurrentLocation(defaultUserLocation); // lấy vị trí mặc định nếu xài máy ảo 
+    setRegion({
+      latitude: defaultUserLocation.latitude,
+      longitude: defaultUserLocation.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+
+    //Lấy vị trí hiện tại của user nếu xài điện thoại
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status === 'granted') {
+      const location = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = location.coords;
+      setCurrentLocation(location.coords);
+
+      setRegion({
+        latitude: latitude,
+        longitude: longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    }
+
     try {
-      const { status } = await Location.requestForegroundPermissionsAsync();
+      const address = await Location.reverseGeocodeAsync(defaultUserLocation);
+      const newFormattedAddress = address
+        .map(
+          (address) =>
+            `${address.streetNumber} ${address.street}, ${address.city}, ${address.region}, ${address.country}`
+        )
+        .join(', ');
 
-      if (status === 'granted') {
-        const location = await Location.getCurrentPositionAsync({});
-        const { latitude, longitude } = location.coords;
-
-        setRegion({
-          latitude: latitude, 
-          longitude: longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421, 
-        });
-
-        const address = await Location.reverseGeocodeAsync({
-          latitude,
-          longitude,
-        });
-
-        const newFormattedAddress = address
-          .map(
-            (address) =>
-              `${address.streetNumber} ${address.street}, ${address.city}, ${address.region}, ${address.country}`
-          )
-          .join(', ');
-
-        setFormattedAddress(newFormattedAddress);
-        console.log('Current Address:', newFormattedAddress);
-      } else {
-        console.log('Permission to access location was denied');
-      }
+      setFormattedAddress(newFormattedAddress);
+      console.log('Current Address:', newFormattedAddress);
     } catch (error) {
-      console.error('Error getting location:', error);
+      console.error('Error getting address:', error);
     }
   };
 
@@ -104,85 +125,248 @@ export default function AddPlace({ route, navigation }) {
 
   const handleFindNearestCinema = async () => {
     try {
-      const defaultLatitude = 10.875331012281416;
-      const defaultLongitude = 106.80024285112276;
+      if (currentLocation) {
+        let nearestCinema = null;
+        let nearestDistance = Infinity;
 
-      setRegion({
-        latitude: defaultLatitude,
-        longitude: defaultLongitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      });
+        cinemaData.forEach(cinema => {
+          const distance = calculateDistance(currentLocation.latitude, currentLocation.longitude, cinema.latitude, cinema.longitude);
+          if (distance < nearestDistance) {
+            nearestDistance = distance;
+            nearestCinema = cinema;
+          }
+        });
 
-      const address = await Location.reverseGeocodeAsync({
-        latitude: defaultLatitude,
-        longitude: defaultLongitude,
-      });
+        if (nearestCinema) {
+          setNearestCinema(nearestCinema);
 
-      const newFormattedAddress = address
-        .map(
-          (address) =>
-            `${address.streetNumber} ${address.street}, ${address.city}, ${address.region}, ${address.country}`
-        )
-        .join(', ');
+          setRegion({
+            latitude: nearestCinema.latitude,
+            longitude: nearestCinema.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          });
 
-      setFormattedAddress(newFormattedAddress);
-      console.log('Default Location Address:', newFormattedAddress);
+          const address = await Location.reverseGeocodeAsync({
+            latitude: nearestCinema.latitude,
+            longitude: nearestCinema.longitude,
+          });
 
+          const newFormattedAddress = address
+            .map(
+              (address) =>
+                `${address.streetNumber} ${address.street}, ${address.city}, ${address.region}, ${address.country}`
+            )
+            .join(', ');
+
+          setFormattedAddress(newFormattedAddress);
+          console.log('Nearest Cinema Address:', newFormattedAddress);
+        } else {
+          console.log('No cinema found');
+        }
+      } else {
+        console.log('Current location not available');
+      }
     } catch (error) {
       console.error('Error finding nearest cinema:', error);
     }
   };
 
+  const handleFindNearbyMovieTheaters = async () => {
+    try {
+      if (currentLocation) {
+        const nearbyTheaters = [];
+
+        cinemaData.forEach(cinema => {
+          const distance = calculateDistance(currentLocation.latitude, currentLocation.longitude, cinema.latitude, cinema.longitude);
+          if (distance <= 20000) { // Check if distance is less than or equal to 20km
+            nearbyTheaters.push(cinema);
+          }
+        });
+
+        if (nearbyTheaters.length > 0) {
+          // Set nearby theaters state
+          setNearestCinema(null);
+
+          // Set markers for nearby theaters
+          const theaterMarkers = nearbyTheaters.map(cinema => ({
+            coordinate: { latitude: cinema.latitude, longitude: cinema.longitude },
+            title: cinema.cinema_name,
+            description: cinema.location_name,
+          }));
+
+          // Update the markers state
+          setTheaterMarkers(theaterMarkers);
+
+          setRegion({
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          });
+
+          const addresses = await Promise.all(nearbyTheaters.map(async (cinema) => {
+            const address = await Location.reverseGeocodeAsync({
+              latitude: cinema.latitude,
+              longitude: cinema.longitude,
+            });
+            return address.map(addr =>
+              `${addr.streetNumber} ${addr.street}, ${addr.city}, ${addr.region}, ${addr.country}`
+            ).join(', ');
+          }));
+
+          const formattedAddresses = addresses.join('\n');
+          console.log('Nearby Movie Theaters Addresses:', formattedAddresses);
+        } else {
+          console.log('No nearby movie theaters found');
+        }
+      } else {
+        console.log('Current location not available');
+      }
+    } catch (error) {
+      console.error('Error finding nearby movie theaters:', error);
+    }
+  };
+
+  const handleShowDirections = () => {
+    if (nearestCinema && currentLocation) {
+      const fetchRoute = async () => {
+        const apiKey = '5b3ce3597851110001cf62484ed9d53e837c4fc695df13f6acd4455a'; // Thay YOUR_API_KEY bằng API key của bạn
+        const url = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${apiKey}&start=${currentLocation.longitude},${currentLocation.latitude}&end=${nearestCinema.longitude},${nearestCinema.latitude}`;
+        
+        try {
+          const response = await axios.get(url);
+          console.log('Đã lấy thông tin đường đi');
+          
+          // Kiểm tra xem response.data có tồn tại và có chứa features hay không
+          if (response.data && response.data.features && response.data.features.length > 0) {
+            const features = response.data.features[0];
+            
+            // Kiểm tra xem features.geometry có tồn tại
+            if (features.geometry && features.geometry.coordinates) {
+              const coords = features.geometry.coordinates.map(point => ({
+                latitude: point[1], // latitude
+                longitude: point[0], // longitude
+              }));
+              setRouteCoords(coords);
+            } else {
+              console.error('Missing geometry in features:', features);
+            }
+          } else {
+            console.error('No features found in response:', response.data);
+          }
+        } catch (error) {
+          console.error('Error fetching route:', error);
+        }
+      };
+  
+      fetchRoute();      
+      setShowDirections(true);
+    } else {
+      console.log('Nearest cinema or current location not available');
+    }
+  };
+
+  // Function to calculate distance between two coordinates
+  const calculateDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 6371e3; // metres
+    const φ1 = (lat1 * Math.PI) / 180; // φ, λ in radians
+    const φ2 = (lat2 * Math.PI) / 180;
+    const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+    const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+    const a =
+      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+      Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c; // in metres
+    return d;
+  };
+
+  const handleDeleteAll = () => {
+    setCurrentLocation(null);
+    setNearestCinema(null);
+    setTheaterMarkers([]);
+    setShowDirections(false);
+  }
+
   return (
     <View style={styles.container}>
-      <View style={{marginTop: 10}}>
-        <View style={{}}>
-
+      <View style={{ marginTop: 10 }}>
+        <View>
           {region ? (
-            <View style={{ width:360, height: 500}}>
-              <MapView style={{ width: 360, height: 500}} region={region}>
-                <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
+            <View style={{ width: 360, height: 400 }}>
+              <MapView style={{ width: 360, height: 400 }} region={region}>
+                {/* Hiển thị Marker cho vị trí hiện tại */}
+                {currentLocation && (
+                  <Marker
+                    coordinate={{ latitude: currentLocation.latitude, longitude: currentLocation.longitude }}
+                    title="Your Location"
+                    description="You are here"
+                  />
+                )}
+
+                {/* Hiển thị Marker cho rạp chiếu phim gần nhất */}
+                {nearestCinema && (
+                  <Marker
+                    coordinate={{ latitude: nearestCinema.latitude, longitude: nearestCinema.longitude }}
+                    title={nearestCinema.cinema_name}
+                    description={nearestCinema.location_name}
+                  />
+                )}
+
+                {/* Hiển thị Markers cho các rạp chiếu phim gần đó */}
+                {theaterMarkers.map((marker, index) => (
+                  <Marker
+                    key={index}
+                    coordinate={marker.coordinate}
+                    title={marker.title}
+                    description={marker.description}
+                  />
+                ))}
+
+                {/* Hiển thị tuyến đường đến rạp chiếu phim gần nhất */}
+                {showDirections && (
+                  <Polyline coordinates={routeCoords} strokeColor="#3498db" strokeWidth={3} />
+                )}
               </MapView>
             </View>
           ) : (
-            <View style={{}}>
+            <View>
               <View style={styles.noImageContainer}>
                 <Text>No location picked yet</Text>
               </View>
             </View>
           )}
         </View>
-        <View style={{ marginTop: 70, flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 15, marginTop: 20, }}>
+        <View style={{ marginTop: 70, flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 15, marginTop: 20 }}>
           <TouchableOpacity onPress={getCurrentLocation}>
-            <View style={styles.greenBorder}> 
-
-              <Image source={require('../assets/map.png')} style={styles.image2}>
-
-              </Image>
+            <View style={styles.greenBorder}>
+              <Image source={require('../assets/map.png')} style={styles.image2} />
               <Text style={styles.text}>Locate User</Text>
             </View>
           </TouchableOpacity>
-
           <TouchableOpacity onPress={() => getCustomLocation(customLatitude, customLongitude)}>
             <View style={styles.greenBorder}>
-
-              <Image source={require('../assets/maps.png')} style={styles.image2}>
-
-              </Image>
+              <Image source={require('../assets/maps.png')} style={styles.image2} />
               <Text style={styles.text}>Pick on Map</Text>
             </View>
           </TouchableOpacity>
         </View>
-
       </View>
-
-
       <View style={{ marginTop: 20 }}>
         <Button title="Find the nearest movie theater" onPress={handleFindNearestCinema}></Button>
       </View>
-
-
+      <View style={{ marginTop: 20 }}>
+        <Button title="Find movie theaters nearby" onPress={handleFindNearbyMovieTheaters}></Button>
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <Button title="Find the shortest route to the nearest cinema" onPress={handleShowDirections}></Button>
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <Button title="Delete All" onPress={handleDeleteAll}></Button>
+      </View>
     </View>
   );
 }
